aac6e71e85939206f59574557fc2b3747c323899
hung.le <hung.le@team.enouvo.com>
2023-09-05 11:08:07 +0700

    fix

--- course-apis/index.js
+++ course-apis/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+var cors = require('cors');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 //App
 const app = express();
+app.use(cors());
 app.get('/', (req, res) => {
   res.send({
     course: ['nodejs', 'reactjs', 'react-native', 'flutter', 'java', 'python'],
